e09ce04aa4447d173d406c62f70df84c757deab1
DVC <kN-dev@DVCs-MacBook-Pro.local>
2023-03-04 10:49:35 +0000

    update: AppConfig.java

--- bankApp/src/main/java/com/dbmsp/bankApp/config/AppConfig.java
+++ bankApp/src/main/java/com/dbmsp/bankApp/config/AppConfig.java
@@ -4,25 +4,23 @@ package com.dbmsp.bankApp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 @Configuration
-@ComponentScan(basePackages = {"com.dbmsp.bankApp"})
-
+@EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
 
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setViewClass(JstlView.class); // added
         viewResolver.setSuffix(".jsp");
-
-        return viewResolver;
+        viewResolver.setViewClass(JstlView.class);
+        registry.viewResolver(viewResolver);
     }
-
-    // other configurations...
 }
+
